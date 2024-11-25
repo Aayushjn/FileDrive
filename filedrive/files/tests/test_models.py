385b7dfdbcbb988be2d23da4fcb6a837e43019c6
gitrahulgit <vrahul200023@gmail.com>
2024-11-25 16:06:26 +0530

    added test for uploaded item deletion

--- filedrive/files/tests/test_models.py
+++ filedrive/files/tests/test_models.py
@@ -22,3 +22,26 @@ def test_uploaded_item_creation():
     assert uploaded_item.file_hash is not None
     assert uploaded_item.owner == user
     uploaded_item.delete(force_policy=HARD_DELETE_NOCASCADE)
+
+
+@pytest.mark.django_db
+def test_uploaded_item_deletion():
+    user = User.objects.create_user(email="test@example.com", password="password123", first_name="John", last_name="Doe")
+    file = SimpleUploadedFile("test.txt", b"Hello, world!", content_type="text/plain")
+    uploaded_item = UploadedItem.objects.create(owner=user, item=file, created_by=user)
+
+    assert Path(uploaded_item.item.name).name.startswith("test")
+    assert uploaded_item.size == len(b"Hello, world!")
+    assert uploaded_item.file_hash is not None
+    assert uploaded_item.owner == user
+    uploaded_item.delete()
+    
+
+    assert UploadedItem.objects.filter(file_hash=uploaded_item.file_hash).first() is None
+    assert UploadedItem.all_objects.filter(file_hash=uploaded_item.file_hash).first() is not None
+
+    uploaded_item.delete(force_policy=HARD_DELETE_NOCASCADE)
+    
+    assert UploadedItem.objects.filter(file_hash=uploaded_item.file_hash).first() is None
+    assert UploadedItem.all_objects.filter(file_hash=uploaded_item.file_hash).first() is None
+
